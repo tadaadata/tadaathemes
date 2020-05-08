0c7f741543d0e2859fb4ac35dc033ab0b30a3825
Lukas Burk <lukas@quantenbrot.de>
2020-05-08 18:09:18 +0200

    Apparently that font loading for windows shouldn't be conditional

--- R/zzz.R
+++ R/zzz.R
@@ -6,15 +6,15 @@
   load_fonts <- getOption("hrbrthemes.loadfonts", default = FALSE) |
     getOption("tadaathemes.loadfonts", default = FALSE)
 
+  if (.Platform$OS.type == "windows")  { # nocov start
+    if (interactive()) packageStartupMessage("Registering Windows fonts with R")
+    extrafont::loadfonts("win", quiet = TRUE)
+  }
+
   if (load_fonts) {
     if (interactive()) packageStartupMessage("Registering PDF & PostScript fonts with R")
     extrafont::loadfonts("pdf", quiet = TRUE)
     extrafont::loadfonts("postscript", quiet = TRUE)
-
-    if (.Platform$OS.type == "windows")  { # nocov start
-      if (interactive()) packageStartupMessage("Registering Windows fonts with R")
-      extrafont::loadfonts("win", quiet = TRUE)
-    }
   }
 
 }

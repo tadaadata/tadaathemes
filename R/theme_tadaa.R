fabb4c27c2c8c518b797c954a06dc09607b139fe
Lukas Burk <lukas@quantenbrot.de>
2020-05-08 14:29:16 +0200

    theme_tadaa[rk]: Add examples

--- R/theme_tadaa.R
+++ R/theme_tadaa.R
@@ -9,37 +9,56 @@
 #'
 #' @import ggplot2
 #' @export
+#' @examples
+#' library(ggplot2)
+#' p <- ggplot(iris, aes(x = Sepal.Width)) +
+#'   geom_histogram(binwidth = .25) +
+#'   labs(
+#'     title = "Yet another iris plot",
+#'     subtitle = "Using theme_tadaa",
+#'     caption = "Oh hi there, didn't see you walk in"
+#'   )
+#'
+#' p + theme_tadaa()
 theme_tadaa <- function(title.size = 16, text.size = 14, legend.position = "top",
-                       show.axis = FALSE, show.grid = TRUE,
-                       plot.margin = c(.8, .4, .4, .8)){
+                        show.axis = FALSE, show.grid = TRUE,
+                        plot.margin = c(.8, .4, .4, .8)) {
   # baseline
   layout <- theme_classic(base_family = "Roboto Condensed")
-  layout <- layout + theme(text = element_text(size = text.size),
-                           title = element_text(size = title.size,
-                                                face = "bold"),
-                           line = element_line(size = .5))
+  layout <- layout + theme(
+    text = element_text(size = text.size),
+    title = element_text(
+      size = title.size,
+      face = "bold"
+    ),
+    line = element_line(size = .5)
+  )
 
   # axis
-  if (inherits(show.axis, "character") | show.axis == FALSE){
-    if (inherits(show.axis, "character")){
+  if (inherits(show.axis, "character") | show.axis == FALSE) {
+    if (inherits(show.axis, "character")) {
       show.axis <- tolower(show.axis)
-      if (show.axis == "x"){
+      if (show.axis == "x") {
         layout <- layout + theme(axis.line.y = element_blank())
       }
-      if (show.axis == "y"){
+      if (show.axis == "y") {
         layout <- layout + theme(axis.line.x = element_blank())
       }
     } else {
-      layout <- layout + theme(axis.line.x = element_blank(),
-                               axis.line.y = element_blank())
+      layout <- layout + theme(
+        axis.line.x = element_blank(),
+        axis.line.y = element_blank()
+      )
     }
   }
 
   # grid lines
-  if (show.grid == TRUE){
-    layout <- layout + theme(panel.grid.major = element_line(size = .2,
-                                                             color = "#333333",
-                                                             linetype = "dotted"))
+  if (show.grid == TRUE) {
+    layout <- layout + theme(panel.grid.major = element_line(
+      size = .2,
+      color = "#333333",
+      linetype = "dotted"
+    ))
   }
 
   # axis titles
@@ -49,23 +68,33 @@ theme_tadaa <- function(title.size = 16, text.size = 14, legend.position = "top"
   layout <- layout + theme(axis.text = element_text(face = "plain"))
 
   # subtitle
-  layout <- layout + theme(plot.subtitle = element_text(face = "plain",
-                                                        color = "#4d4d4d"))
+  layout <- layout + theme(plot.subtitle = element_text(
+    face = "plain",
+    color = "#4d4d4d"
+  ))
 
   # legend
-  layout <- layout + theme(legend.position = legend.position,
-                           legend.background = element_blank(),
-                           legend.key.height = unit(2, "line"))
+  layout <- layout + theme(
+    legend.position = legend.position,
+    legend.background = element_blank(),
+    legend.key.height = unit(2, "line")
+  )
 
   # facets
-  layout <- layout + theme(strip.background = element_blank(),
-                           strip.text = element_text(size = title.size,
-                                                     face = "plain"))
+  layout <- layout + theme(
+    strip.background = element_blank(),
+    strip.text = element_text(
+      size = title.size,
+      face = "plain"
+    )
+  )
 
   # misc
-  layout <- layout + theme(panel.background = element_rect(fill = "white"),
-                           plot.background = element_rect(fill = "white"),
-                           plot.margin = unit(plot.margin, "cm"))
+  layout <- layout + theme(
+    panel.background = element_rect(fill = "white"),
+    plot.background = element_rect(fill = "white"),
+    plot.margin = unit(plot.margin, "cm")
+  )
 
   layout
 }
--- R/theme_tadaark.R
+++ R/theme_tadaark.R
@@ -18,87 +18,132 @@
 #'
 #' @import ggplot2
 #' @export
+#' @examples
+#' library(ggplot2)
+#' p <- ggplot(iris, aes(x = Sepal.Width)) +
+#'   geom_histogram(binwidth = .25) +
+#'   labs(
+#'     title = "Yet another iris plot",
+#'     subtitle = "Using theme_tadaark",
+#'     caption = "Oh hi there, didn't see you walk in"
+#'   )
+#'
+#' p + theme_tadaark()
 theme_tadaark <- function(title.size = 16, text.size = 14, legend.position = "top",
-                         show.axis = FALSE, show.grid = TRUE,
-                         plot.margin = c(.8, .4, .4, .8)){
-    # baseline
-    linecolor <- "#4d4d4d"
-    textcolor <- "#d9d9d9"
-    layout <- theme_classic(base_family = "Klavika Regular")
-    layout <- layout + theme(text = element_text(size = text.size,
-                                                 color = textcolor),
-                             title = element_text(size = title.size,
-                                                  face = "bold",
-                                                  color = textcolor),
-                             line = element_line(size = .5,
-                                                 color = linecolor))
+                          show.axis = FALSE, show.grid = TRUE,
+                          plot.margin = c(.8, .4, .4, .8)) {
+  # baseline
+  linecolor <- "#4d4d4d"
+  textcolor <- "#d9d9d9"
+  layout <- theme_classic(base_family = "Klavika Regular")
+  layout <- layout + theme(
+    text = element_text(
+      size = text.size,
+      color = textcolor
+    ),
+    title = element_text(
+      size = title.size,
+      face = "bold",
+      color = textcolor
+    ),
+    line = element_line(
+      size = .5,
+      color = linecolor
+    )
+  )
 
-    # override geom defaults
-    new_def <- "#00b0a0"
-    update_geom_defaults("point", list(colour = new_def))
-    update_geom_defaults("line", list(colour = new_def))
-    update_geom_defaults("area", list(colour = new_def,
-                                      fill   = new_def))
-    update_geom_defaults("rect", list(colour = new_def,
-                                      fill   = new_def))
-    update_geom_defaults("density", list(colour = new_def,
-                                         fill   = new_def))
-    update_geom_defaults("bar", list(colour = new_def,
-                                     fill   = new_def))
-    update_geom_defaults("col", list(colour = new_def,
-                                     fill   = new_def))
-    update_geom_defaults("text", list(colour = new_def))
+  # override geom defaults
+  new_def <- "#00b0a0"
+  update_geom_defaults("point", list(colour = new_def))
+  update_geom_defaults("line", list(colour = new_def))
+  update_geom_defaults("area", list(
+    colour = new_def,
+    fill = new_def
+  ))
+  update_geom_defaults("rect", list(
+    colour = new_def,
+    fill = new_def
+  ))
+  update_geom_defaults("density", list(
+    colour = new_def,
+    fill = new_def
+  ))
+  update_geom_defaults("bar", list(
+    colour = new_def,
+    fill = new_def
+  ))
+  update_geom_defaults("col", list(
+    colour = new_def,
+    fill = new_def
+  ))
+  update_geom_defaults("text", list(colour = new_def))
 
-    # axis
-    layout <- layout + theme(axis.line = element_line(color = "#d9d9d9"))
-    if (inherits(show.axis, "character") | show.axis == FALSE){
-        if (inherits(show.axis, "character")){
-            show.axis <- tolower(show.axis)
-            if (show.axis == "x"){
-                layout <- layout + theme(axis.line.y = element_blank())
-            }
-            if (show.axis == "y"){
-                layout <- layout + theme(axis.line.x = element_blank())
-            }
-        } else {
-            layout <- layout + theme(axis.line.x = element_blank(),
-                                     axis.line.y = element_blank())
-        }
+  # axis
+  layout <- layout + theme(axis.line = element_line(color = "#d9d9d9"))
+  if (inherits(show.axis, "character") | show.axis == FALSE) {
+    if (inherits(show.axis, "character")) {
+      show.axis <- tolower(show.axis)
+      if (show.axis == "x") {
+        layout <- layout + theme(axis.line.y = element_blank())
+      }
+      if (show.axis == "y") {
+        layout <- layout + theme(axis.line.x = element_blank())
+      }
+    } else {
+      layout <- layout + theme(
+        axis.line.x = element_blank(),
+        axis.line.y = element_blank()
+      )
     }
+  }
 
-    # grid lines
-    if (show.grid == TRUE){
-        layout <- layout + theme(panel.grid.major = element_line(size = .2,
-                                                                 color = linecolor,
-                                                                 linetype = "dotted"))
-    }
+  # grid lines
+  if (show.grid == TRUE) {
+    layout <- layout + theme(panel.grid.major = element_line(
+      size = .2,
+      color = linecolor,
+      linetype = "dotted"
+    ))
+  }
 
-    # subtitle
-    layout <- layout + theme(plot.subtitle = element_text(face = "plain",
-                                                          color = "#8c8c8c"))
+  # subtitle
+  layout <- layout + theme(plot.subtitle = element_text(
+    face = "plain",
+    color = "#8c8c8c"
+  ))
 
-    # axis titles
-    layout <- layout + theme(axis.title.x = element_text(margin = margin(t = 8)))
+  # axis titles
+  layout <- layout + theme(axis.title.x = element_text(margin = margin(t = 8)))
 
-    # axis labels
-    layout <- layout + theme(axis.text = element_text(face = "plain",
-                                                      color = "#8c8c8c"))
+  # axis labels
+  layout <- layout + theme(axis.text = element_text(
+    face = "plain",
+    color = "#8c8c8c"
+  ))
 
-    # legend
-    layout <- layout + theme(legend.position = legend.position,
-                             legend.background = element_blank(),
-                             legend.key.height = unit(2, "line"))
+  # legend
+  layout <- layout + theme(
+    legend.position = legend.position,
+    legend.background = element_blank(),
+    legend.key.height = unit(2, "line")
+  )
 
-    # facets
-    layout <- layout + theme(strip.background = element_blank(),
-                             strip.text = element_text(size = title.size,
-                                                       color = textcolor,
-                                                       face = "plain"))
+  # facets
+  layout <- layout + theme(
+    strip.background = element_blank(),
+    strip.text = element_text(
+      size = title.size,
+      color = textcolor,
+      face = "plain"
+    )
+  )
 
-    # misc
-    layout <- layout + theme(panel.background = element_rect(fill = "#181818"),
-                             plot.background = element_rect(fill = "#181818"), # or: #1a1a1a; OR #191919?!
-                             plot.margin = unit(plot.margin, "cm"))
+  # misc
+  layout <- layout + theme(
+    panel.background = element_rect(fill = "#181818"),
+    plot.background = element_rect(fill = "#181818"), # or: #1a1a1a; OR #191919?!
+    plot.margin = unit(plot.margin, "cm")
+  )
 
-    layout
+  layout
 }
